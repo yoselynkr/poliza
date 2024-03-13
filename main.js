cc82f28cfa81859c144017a45883919196abcd77
yoselynkr <157842421+yoselynkr@users.noreply.github.com>
2024-03-13 00:07:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+class poliza {
+    constructor(id, mA, p) {
+        this.identificacion = id;
+        this.montoAsegurado = mA;
+        this.porcentaje = p;
+    }
+}
+class vida extends poliza {
+    constructor(id, mA, p, e) {
+        super(id, mA, p);
+        this.edad = e;
+    }
+    cobertura() {
+        return this.montoAsegurado * this.porcentaje
+    }
+    montoPagar() {
+        if (this.edad <= 18)
+            return this.cobertura() * 0.05
+        else
+            return this.cobertura()
+    }
+
+}
+
+class vehiculo extends poliza {
+    constructor(id, mA, p, a) {
+        super(id, mA, p)
+        this.anno = a
+    }
+    cobertura() {
+        return this.montoAsegurado * this.porcentaje
+    }
+
+    montoPagar() {
+        if (this.anno <= 2000)
+            return this.cobertura() * 0.07
+        else
+            return this.cobertura() * 0.12
+    }
+
+}
+
+class aseguradora {
+constructor(cP,aM){
+    this.contPoliza = cP
+    this.acumMontoPagar = aM
+}
+
+porcesar(p){
+    this.acumMontoPagar += p.montoPagar()
+    this.contPoliza++
+}
+
+promedio(){
+    return this.acumMontoPagar / this.contPoliza
+}
+ 
+}
